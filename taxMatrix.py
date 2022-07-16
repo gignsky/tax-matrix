bf1b291cabfb953ca4fbf103a12c1539b49fedf7
gignsky <gignsky@gmail.com>
2022-07-16 14:28:11 -0400

    added program outline

---
+++ taxMatrix.py
@@ -0,0 +1,33 @@
+# max 7.16.22 tax matrix conversion from the excel workbook
+
+
+
+def main():
+    # print a welcome
+
+    # begin loop to allow continued editing until everything looks right; include ability to save and restart at any point (maybe)
+
+        # get price
+
+        # begin county loop for same reason as above
+
+            # select county
+
+            # enter that counties sheet while setting output vars for statistics and final output
+
+        # print stats and final output
+
+        #ensure user wishes to output this text as final
+
+            #if no reset with ability to use old price and stats
+
+            #else end thank you and print final statement
+
+
+
+
+
+
+###
+main()
+###
