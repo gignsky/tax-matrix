e4e04478db69da65a3f1a42c3b3b49330d79ad9e
gignsky <gignsky@gmail.com>
2022-11-21 21:24:23 -0500

    configured new option for sc residency in main menu

--- tax_matrix/main.py
+++ tax_matrix/main.py
@@ -43,10 +43,11 @@ def main():
         0: "Quit Program & Output Statement",
         1: "Print Statisticts & Statement\n...",
         2: "Modify Price\n...",
-        3: "Modify County",
-        4: "Modify Countywide Police, Fire, and/or EMS rates",
-        5: "Modify County Special Options - Fees & Rates\n...",
-        6: "Modify City and/or City Options\n...",
+        3: "Modify County\n...",
+        4: "Modify Residential Status (Primary Residence / Investment)",
+        5: "Modify Countywide Police, Fire, and/or EMS rates",
+        6: "Modify County Special Options - Fees & Rates\n...",
+        7: "Modify City and/or City Options\n...",
         99: "Reload Counties to Default Values",
     }
 
@@ -66,7 +67,7 @@ def main():
 
         else:
             inital_run_status, menu_option = inital_run_stack(
-                inital_run_status, options_dict
+                inital_run_status, options_dict, subject
             )
 
         # "Quit Program & Output Statement"
@@ -121,17 +122,30 @@ def main():
 
                 cls()
 
-        # "Modify Countywide Police, Fire, and/or EMS rates"
+        # Modify Residency for SC ONLY
         elif menu_option == options_dict[4]:
+            if subject.county.get_state() == "SC":
+                residency_bool = src.utilities.inputs.InputHelper.grab_residency()
+                subject.set_residency(residency_bool)
+            else:
+                src.utilities.printers.Printer.print_red(
+                    "Subject is not subject to this question due to not being located in SC"
+                )
+
+                src.utilities.logic.LogicalWork.wait()
+
+                cls()
+        # "Modify Countywide Police, Fire, and/or EMS rates"
+        elif menu_option == options_dict[5]:
             subject.county.select_countywide_services()
 
         # "Modify County Special Options"
-        elif menu_option == options_dict[5]:
+        elif menu_option == options_dict[6]:
             # check for special options
             subject.county.modify_special_options()
 
         # "Modify City and/or City Options"
-        elif menu_option == options_dict[6]:
+        elif menu_option == options_dict[7]:
             if inital_run_status == "continue onwards":
                 pick_city_bool = src.utilities.inputs.InputHelper.choice_bool(
                     "Do you want to select a city at this time?"
@@ -224,7 +238,7 @@ def generate_statement(subject):
     return statement
 
 
-def inital_run_stack(inital_run_status, options_dict):
+def inital_run_stack(inital_run_status, options_dict, subject_class):
     """
     inital_run_stack runs menu selection for inital run of script
 
@@ -241,15 +255,22 @@ def inital_run_stack(inital_run_status, options_dict):
         inital_run_status = "Get Price then County"
     elif inital_run_status == "Get Price then County":
         menu_option = options_dict[3]
-        inital_run_status = "check special district options"
+        inital_run_status = "sc_residency_check"
+    elif inital_run_status == "sc_residency_check":
+        if subject_class.county.get_state() == "SC":
+            menu_option = options_dict[4]
+            inital_run_status = "check special district options"
+        else:
+            menu_option = options_dict[5]
+            inital_run_status = "Modify Countywide Fire, Police, & EMS"
     elif inital_run_status == "check special district options":
-        menu_option = options_dict[4]
+        menu_option = options_dict[5]
         inital_run_status = "Modify Countywide Fire, Police, & EMS"
     elif inital_run_status == "Modify Countywide Fire, Police, & EMS":
-        menu_option = options_dict[5]
+        menu_option = options_dict[6]
         inital_run_status = "Do you want a city?"
     elif inital_run_status == "Do you want a city?":
-        menu_option = options_dict[6]
+        menu_option = options_dict[7]
         inital_run_status = "continue onwards"
     else:
         menu_option = options_dict[1]  # print stats before continuing
