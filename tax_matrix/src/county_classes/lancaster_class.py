c07f8fa5c9246cf01c91adca01bec5cae8caffc5
gignsky <gignsky@gmail.com>
2022-11-22 21:31:29 -0500

    updated lancaster class to remove old work that is now unneccary for old tax years

--- tax_matrix/src/county_classes/lancaster_class.py
+++ tax_matrix/src/county_classes/lancaster_class.py
@@ -32,8 +32,7 @@ class LancasterCo(general_classes.County):
         county_wide_ems_rate,
         cities,
         special_stuff,
-        # special_fees_per_items,
-        # sales_tax_credit_factors,
+        sales_tax_credit_factors,
     ):
         super().__init__(
             county_name,
@@ -49,214 +48,16 @@ class LancasterCo(general_classes.County):
             cities,
             special_stuff,
         )
+        # TODO UNCOMMENT WHEN THING IS WORKED ON
+        self.stcf_dict = sales_tax_credit_factors
 
-    #         # set & configure special dicts
-    #         self.special_fees_per_items_dict = special_fees_per_items
-    #         (
-    #             self.special_fees_per_items_dict_titles,
-    #             self.special_fees_per_items_dict_fees,
-    #             self.special_fees_per_items_dict_item_types,
-    #             self.special_fees_per_items_dict_items,
-    #             self.special_fees_per_items_dict_items_multiple_rates,
-    #             self.special_fees_per_items_dict_items_default_multiple_rate,
-    #             self.special_fees_per_items_dict_items_default_fee,
-    #             self.special_fees_per_items_dict_items_multiple_str,
-    #         ) = self.separate_per_items(self.special_fees_per_items_dict)
-    #         self.sales_tax_credit_factors_master_dict = sales_tax_credit_factors
-    #
-    #         # set self.mod_dictionary
-    #         self.mod_dictionary = {}
-    #
-    #     # TODO CONVERT TO CLASS
-    #     # configuration of special dict methods
-    #     def separate_per_items(self, dictionary):
-    #         titles = []
-    #         fees = []
-    #         item_types = []
-    #         items = []
-    #         multiple_values = []
-    #         default_multiple_values = []
-    #         default_fees = []
-    #         multiple_value_strs = []
-    #         for i in dictionary:
-    #             # grab title
-    #             inner_dictionary = dictionary[i]
-    #             title = list(inner_dictionary.keys())[0]
-    #             titles.append(title)
-    #
-    #             # grab key
-    #             key_dict = inner_dictionary[title]
-    #
-    #             # tuple item zero
-    #             dict_0 = key_dict[0]
-    #
-    #             # tuple item one
-    #             dict_1 = key_dict[1]
-    #
-    #             # get fee amnt
-    #             fee_amnt = dict_0["fee"]
-    #
-    #             # set fee amnt
-    #             fees.append(None)
-    #             default_fees.append(fee_amnt)
-    #
-    #             # get item type
-    #             item_type = list(dict_1.keys())[0]
-    #
-    #             # set item type
-    #             item_types.append(item_type)
-    #
-    #             # get multiple rate value
-    #             multiple_rate = dict_1[item_type]
-    #
-    #             # set multiple rate type
-    #             multiple_values.append(None)
-    #             default_multiple_values.append(multiple_rate)
-    #
-    #             # set item default value of 99999 for error throwing
-    #             items.append(999999)
-    #
-    #             # set string to none
-    #             multiple_value_strs.append(None)
-    #         return (
-    #             titles,
-    #             fees,
-    #             item_types,
-    #             items,
-    #             multiple_values,
-    #             default_multiple_values,
-    #             default_fees,
-    #             multiple_value_strs,
-    #         )
+    def modify_special_options(self):
 
-    # deal with special options
-    # def modify_special_options(self):
-    #     super().modify_special_options()
+        #run base command
+        super()
 
+        #present option to continue with sales tax credit options
+        looper=LogicalWork.choice_bool("Would you like to modify Sales Tax Credit Factors?")
 
-#
-#         # per item fees
-#         self.modify_per_item_options()
-#
-#         # TODO UNCOMMENT WHEN THING IS WORKED ON
-#         # # tax credit options
-#         # self.modify_tax_credit_options()
-#
-#     # modify
-#     def modify_per_item_options(self):
-#         do_modify = InputHelper.choice_bool(
-#             "Would you like to apply special options that require per item multiplications?"
-#         )
-#
-#         if do_modify:
-#             looping = True
-#
-#             while looping:
-#                 self.mod_dictionary = self.create_per_item_options_dictionary()
-#                 input_return = InputHelper.input_from_dict(
-#                     self.mod_dictionary, "Which Option would you like to modify?"
-#                 )
-#
-#                 if input_return == self.mod_dictionary[0]:
-#                     looping = False
-#                 elif input_return == self.mod_dictionary[1]:
-#                     self.modify_per_item(0)
-#                 elif input_return == self.mod_dictionary[2]:
-#                     self.modify_per_item(1)
-#                 else:
-#                     debugpy.breakpoint()
-#
-#     ### Mod per item helper
-#     def modify_per_item(self, index_value):
-#         Printer.liner()
-#         print(self.mod_dictionary[index_value])
-#         Printer.liner()
-#         enable = InputHelper.choice_bool("Do you wish to enable this item?")
-#
-#         if enable:
-#             self.special_fees_per_items_dict_fees[
-#                 index_value
-#             ] = self.special_fees_per_items_dict_fees[index_value]
-#
-#             Printer.liner()
-#             Printer.print_green(
-#                 f"{self.special_fees_per_items_dict_titles[index_value]} has been enabled with a fee of ${self.special_fees_per_items_dict_fees[index_value]}..."
-#             )
-#             Printer.short_liner()
-#
-#             if (
-#                 self.special_fees_per_items_dict_items_multiple_rates[index_value]
-#                 is None
-#             ):
-#                 input_int, input_str = InputHelper.per_item_item_input(
-#                     self.special_fees_per_items_dict_item_types[index_value],
-#                     self.special_fees_per_items_dict_items_default_multiple_rate[
-#                         index_value
-#                     ],
-#                     self.special_fees_per_items_dict_items_multiple_rates[index_value],
-#                 )
-#
-#                 self.apply_special_fee_per_item_value(index_value, input_int, input_str)
-#             else:
-#                 choice_return = InputHelper.choice_bool(
-#                     f"Do you wish to modify the current value of {self.special_fees_per_items_dict_items_multiple_rates[index_value]} {self.special_fees_per_items_dict_item_types[index_value]}(s)?"
-#                 )
-#                 if choice_return:
-#                     input_int, input_str = InputHelper.per_item_item_input(
-#                         self.special_fees_per_items_dict_item_types[index_value],
-#                         self.special_fees_per_items_dict_items_default_multiple_rate[
-#                             index_value
-#                         ],
-#                         self.special_fees_per_items_dict_items_multiple_rates[
-#                             index_value
-#                         ],
-#                     )
-#
-#                     self.apply_special_fee_per_item_value(
-#                         index_value, input_int, input_str
-#                     )
-#         else:
-#             self.special_fees_per_items_dict_fees[index_value] = None
-#
-#     # apply value
-#     def apply_special_fee_per_item_value(self, index_value, int_value, str_value):
-#         self.special_fees_per_items_dict_items_multiple_rates[index_value] = int_value
-#         self.special_fees_per_items_dict_items_multiple_str[index_value] = str_value
-#
-#     # generate dictionary options
-#     def create_per_item_options_dictionary(self):
-#         dict_to_return = {}
-#         looping = True
-#         index_value = 0
-#
-#         while looping:
-#             if index_value == 0:
-#                 dict_to_return[index_value] = "Quit"
-#                 index_value += 1
-#             else:
-#                 for (
-#                     title,
-#                     fee,
-#                     item_type,
-#                     item,
-#                     multiple,
-#                     default_multiple,
-#                     default_fee,
-#                 ) in zip(
-#                     self.special_fees_per_items_dict_titles,
-#                     self.special_fees_per_items_dict_fees,
-#                     self.special_fees_per_items_dict_item_types,
-#                     self.special_fees_per_items_dict_items,
-#                     self.special_fees_per_items_dict_items_multiple_rates,
-#                     self.special_fees_per_items_dict_items_default_multiple_rate,
-#                     self.special_fees_per_items_dict_items_default_fee,
-#                 ):
-#                     dict_to_return[
-#                         index_value
-#                     ] = f"{title} - Fee: ${fee} - {item_type}: {item} | {multiple} {item_type}(s) | DEFAULT FEE: ${default_fee} - Per {default_multiple} {item_type}(s)"
-#                     index_value += 1
-#                 looping = False
-#
-#         return dict_to_return
-
-# TODO Integrate residency with statement
+        while looper:
+            
