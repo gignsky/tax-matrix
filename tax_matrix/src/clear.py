a7702967d33c77d59cdb1f3a1a4d2a30ad205956
gignsky <gignsky@gmail.com>
2023-03-01 16:33:10 -0500

    Revert "clarifies doc string"
    This reverts commit 7b3f66a1b6b297e152f7888bb4b45e21aa66b0ed.

--- tax_matrix/src/clear.py
+++ tax_matrix/src/clear.py
@@ -7,13 +7,7 @@ from . import os
 
 def cls():
     """
-    Clears the command outline on the screen.
-
-    This function uses the `os` module to run the `cls` command on Windows systems
-    or the `clear` command on non-Windows systems.
-
-    Example:
-        cls()
+    cls will clear command outline
     """
     os.system("cls" if os.name == "nt" else "clear")
     # use "cls()" to clear screen
