7b3f66a1b6b297e152f7888bb4b45e21aa66b0ed
gignsky <gignsky@gmail.com>
2023-03-01 16:31:01 -0500

    clarifies doc string

--- tax_matrix/src/clear.py
+++ tax_matrix/src/clear.py
@@ -7,7 +7,13 @@ from . import os
 
 def cls():
     """
-    cls will clear command outline
+    Clears the command outline on the screen.
+
+    This function uses the `os` module to run the `cls` command on Windows systems
+    or the `clear` command on non-Windows systems.
+
+    Example:
+        cls()
     """
     os.system("cls" if os.name == "nt" else "clear")
     # use "cls()" to clear screen
