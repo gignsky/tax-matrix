535e66a64907c13814b19a1366854b52e00dcafe
gignsky <gignsky@gmail.com>
2024-02-03 18:20:04 -0500

    Update submit button label and add event handling for county data display pre nicegui

--- tax_matrix/main.py
+++ tax_matrix/main.py
@@ -19,7 +19,7 @@ layout = [
     [
         sg.Text("Subject Value/Purchase Price"),
         sg.InputText(key="-PURCHASE_PRICE-", enable_events=True),
-        sg.Button("Submit", key="-SUBMIT-"),
+        sg.Button("Submit", key="-SUBMIT PURCHASE PRICE-"),
     ],
     [sg.HorizontalSeparator(color="white")],
     [
@@ -37,12 +37,16 @@ while True:
     event, values = window.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
-    if event == "-SUBMIT-":
+    if event == "-SUBMIT PURCHASE PRICE-":
         try:
             purchase_price = int(values["-PURCHASE_PRICE-"])
             formatted_price = f"${purchase_price:,}"
             window["-PURCHASE_PRICE-"].update(formatted_price)
         except ValueError:
             pass
-
+    if event.startswith("-") and event.endswith("-"):
+        county_name = event[1:-1].lower()
+        county_data = counties.get(county_name)
+        if county_data:
+            src.county_data.county_gui.display_county_data(county_data)
 window.close()
