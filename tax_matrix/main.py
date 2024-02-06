aee4d927d2b2f221cadf5e8b2da76e029ba374fb
gignsky <gignsky@gmail.com>
2024-02-05 19:13:01 -0500

    Refactor main.py and PythonImportHelper-v2-Completion.json

--- tax_matrix/main.py
+++ tax_matrix/main.py
@@ -18,19 +18,48 @@ def main(page: ft.Page):
     page.update()
 
     def submit_value(e):
-        page.add(ft.Text(value=f"Entered Value: ${value.value}"))
+        # page.add(ft.Text(value=f"Entered Value: ${value.value}"))
+        used_value.value = f"Subject Value being used: ${value_text_box.value}"
+        page.update()
 
-    #     t = ft.Text()
-    #     page.add(t)  # it's a shortcut for page.controls.append(t) and then page.update()
-    #
-    #     for i in range(10):
-    #         t.value = f"Step {i}"
-    #         page.update()
-    #         time.sleep(1)
+    def choose_county(e):
+        # page.add(ft.Text(value=f"Selected County: {county_dropdown.value}"))
+        selected_county.value = f"Selected County: {county_dropdown.value}"
+        page.update()
 
-    value = ft.TextField(hint_text="Enter Subject Value")
+    # declare value field for entering
+    value_text_box = ft.TextField(
+        hint_text="Enter Subject Value", value="", text_align="right"
+    )
 
-    page.add(ft.Row([value, ft.ElevatedButton("Submit Value", on_click=submit_value)]))
+    # declare value field for displaying
+    used_value = ft.Text(value=f"Subject Value being used: ${value_text_box.value}")
+    page.add(used_value)
 
+    list_of_county_dropdown_options = []
+    # county options
+    for county in list(counties):
+        list_of_county_dropdown_options.append(ft.dropdown.Option(county))
+
+    # declare county selector
+    county_dropdown = ft.Dropdown(
+        options=list_of_county_dropdown_options,
+        on_change=choose_county,
+        width=400,
+    )
+
+    # declare selected county field
+    selected_county = ft.Text(value=f"Selected County: {county_dropdown.value}")
+    page.add(selected_county)
+
+    page.add(
+        ft.Row(
+            [value_text_box, ft.ElevatedButton("Submit Value", on_click=submit_value)]
+        )
+    )
+    page.add(
+        ft.Text(value="Please select a county: "),
+    )
+    page.add(county_dropdown)
 
 ft.app(target=main)
